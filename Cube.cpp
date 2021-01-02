4d5bdcf5d4c47b9d6d4672028f0819bf83395315
JakubPrzystasz <jakub.przystasz@gmail.com>
2021-01-02 15:25:03 +0100

    Fix 0-axis bug

--- Cube.cpp
+++ Cube.cpp
@@ -1,55 +1,55 @@
 #include "Cube.h"
 
 Face Cube::FrontFace = Face(
-	Vertex(glm::vec3(0.5f, 0.5f, 0.5f), glm::vec2(1.0f, 1.0f)), //right top
-	Vertex(glm::vec3(0.5f, -0.5f, 0.5f), glm::vec2(1.0f, 0.0f)),//right bottom
-	Vertex(glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec2(0.0f, 0.0f)),//left bottom
-	Vertex(glm::vec3(-0.5f, 0.5f, 0.5f), glm::vec2(0.0f, 1.0f)),//left top
+	Vertex(glm::vec3(1, 1, 1), glm::vec2(1.0f, 1.0f)), //right top
+	Vertex(glm::vec3(1, 0, 1), glm::vec2(1.0f, 0.0f)),//right bottom
+	Vertex(glm::vec3(0, 0, 1), glm::vec2(0.0f, 0.0f)),//left bottom
+	Vertex(glm::vec3(0, 1, 1), glm::vec2(0.0f, 1.0f)),//left top
 	1, 2, 3, // right bottom -> left bottom -> left top 
 	0, 1, 3 // right top -> right bottom -> left top
 );
 
 Face Cube::BackFace = Face(
-	Vertex(glm::vec3(0.5f, 0.5f, -0.5f), glm::vec2(0.0f, 1.0f)),//left top
-	Vertex(glm::vec3(0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 0.0f)),//left bottom
-	Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec2(1.0f, 0.0f)),//right bottom
-	Vertex(glm::vec3(-0.5f, 0.5f, -0.5f), glm::vec2(1.0f, 1.0f)),//right top
+	Vertex(glm::vec3(1, 1, 0), glm::vec2(0.0f, 1.0f)),//left top
+	Vertex(glm::vec3(1, 0, 0), glm::vec2(0.0f, 0.0f)),//left bottom
+	Vertex(glm::vec3(0, 0, 0), glm::vec2(1.0f, 0.0f)),//right bottom
+	Vertex(glm::vec3(0, 1, 0), glm::vec2(1.0f, 1.0f)),//right top
 	1, 2, 3, // right bottom -> left bottom -> left top 
 	0, 1, 3  // right top -> right bottom -> left top
 );
 
 Face Cube::TopFace = Face(
-	Vertex(glm::vec3(0.5f, 0.5f, -0.5f), glm::vec2(1.0f, 1.0f)),//right top
-	Vertex(glm::vec3(0.5f, 0.5f, 0.5f), glm::vec2(1.0f, 0.0f)),//right bottom
-	Vertex(glm::vec3(-0.5f, 0.5f, 0.5f), glm::vec2(0.0f, 0.0f)),//left bottom
-	Vertex(glm::vec3(-0.5f, 0.5f, -0.5f), glm::vec2(0.0f, 1.0f)),//left top
+	Vertex(glm::vec3(1, 1, 0), glm::vec2(1.0f, 1.0f)),//right top
+	Vertex(glm::vec3(1, 1, 1), glm::vec2(1.0f, 0.0f)),//right bottom
+	Vertex(glm::vec3(0, 1, 1), glm::vec2(0.0f, 0.0f)),//left bottom
+	Vertex(glm::vec3(0, 1, 0), glm::vec2(0.0f, 1.0f)),//left top
 	1, 2, 3, // right bottom -> left bottom -> left top 
 	0, 1, 3  // right top -> right bottom -> left top
 );
 
 Face Cube::BottomFace = Face(
-	Vertex(glm::vec3(0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 1.0f)),//right top
-	Vertex(glm::vec3(0.5f, -0.5f, 0.5f), glm::vec2(0.0f, 0.0f)),//right bottom
-	Vertex(glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec2(1.0f, 0.0f)),//left bottom
-	Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec2(1.0f, 1.0f)),//left top
+	Vertex(glm::vec3(1, 0, 0), glm::vec2(0.0f, 1.0f)),//right top
+	Vertex(glm::vec3(1, 0, 1), glm::vec2(0.0f, 0.0f)),//right bottom
+	Vertex(glm::vec3(0, 0, 1), glm::vec2(1.0f, 0.0f)),//left bottom
+	Vertex(glm::vec3(0, 0, 0), glm::vec2(1.0f, 1.0f)),//left top
 	1, 2, 3, // right bottom -> left bottom -> left top 
 	0, 1, 3  // right top -> right bottom -> left top
 );
 
 Face Cube::RightFace = Face(
-	Vertex(glm::vec3(0.5f, 0.5f, -0.5f), glm::vec2(1.0f, 1.0f)),//right top
-	Vertex(glm::vec3(0.5f, -0.5f, -0.5f), glm::vec2(1.0f, 0.0f)),//right bottom
-	Vertex(glm::vec3(0.5f, -0.5f, 0.5f), glm::vec2(0.0f, 0.0f)),//left bottom
-	Vertex(glm::vec3(0.5f, 0.5f, 0.5f), glm::vec2(0.0f, 1.0f)),//left top
+	Vertex(glm::vec3(1, 1, 0), glm::vec2(1.0f, 1.0f)),//right top
+	Vertex(glm::vec3(1, 0, 0), glm::vec2(1.0f, 0.0f)),//right bottom
+	Vertex(glm::vec3(1, 0, 1), glm::vec2(0.0f, 0.0f)),//left bottom
+	Vertex(glm::vec3(1, 1, 1), glm::vec2(0.0f, 1.0f)),//left top
 	1, 2, 3, // right bottom -> left bottom -> left top 
 	0, 1, 3  // right top -> right bottom -> left top
 );
 
 Face Cube::LeftFace = Face(
-	Vertex(glm::vec3(-0.5f, 0.5f, 0.5f), glm::vec2(1.0f, 1.0f)),//right top
-	Vertex(glm::vec3(-0.5f, -0.5f, 0.5f), glm::vec2(1.0f, 0.0f)),//right bottom
-	Vertex(glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec2(0.0f, 0.0f)),//left bottom
-	Vertex(glm::vec3(-0.5f, 0.5f, -0.5f), glm::vec2(0.0f, 1.0f)),//left top
+	Vertex(glm::vec3(0, 1, 1), glm::vec2(1.0f, 1.0f)),//right top
+	Vertex(glm::vec3(0, 0, 1), glm::vec2(1.0f, 0.0f)),//right bottom
+	Vertex(glm::vec3(0, 0, 0), glm::vec2(0.0f, 0.0f)),//left bottom
+	Vertex(glm::vec3(0, 1, 0), glm::vec2(0.0f, 1.0f)),//left top
 	1, 2, 3, // right bottom -> left bottom -> left top 
 	0, 1, 3  // right top -> right bottom -> left top
 );
